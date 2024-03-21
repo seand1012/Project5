5ae0c5a610e5a9276e34023d133ab099c16aee79
seand1012 <146482091+seand1012@users.noreply.github.com>
2024-03-21 16:18:13 -0500

    Update sysproc.c

--- sysproc.c
+++ sysproc.c
@@ -108,15 +108,48 @@ sys_uptime(void)
 
 int
 sys_macquire(void){
+  mutex* m;
+  if(argptr(0, (void*)&m, sizeof(*m) < 0)){
+    return -1;
+  }
+  acquire(m->lock);
+
+  while(m->lock->locked){
+    sleep(m, m->lock);
+  }
+
+  m->lock->locked = 1;
+  m->lock->cpu = mycpu();
+
+  release(m->lock);
   return 0;
 }
 
 int
 sys_mrelease(void){
+  mutex* m;
+  if(argptr(0, (void*)&m, sizeof(*m) != 0)){
+    return -1;
+  }
+  
+  acquire(m->lock);
+
+  m->lock->locked = 0;
+  m->lock->cpu = (void*)0;
+  wakeup(m);
+
+  release(m->lock);
   return 0;
 }
 
 int
 sys_nice(void){
+  int inc;
+  struct proc *p = myproc();
+  if(argint(0, &inc) < 0){
+    return -1;
+  }
+
+
   return 0;
 }
